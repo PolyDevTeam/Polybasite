8fcf8d1f39c0b36ddb1a5396744043728f01158f
Guillaume Michon <slydevis@hotmail.fr>
2017-12-13 15:58:59 +0100

    Add Basite generation

--- src/Basite.cpp
+++ src/Basite.cpp
@@ -5,6 +5,10 @@
 #include "Basite.hpp"
 #include "Color.hpp"
 
+Basite::Basite(unsigned x, unsigned y, unsigned power) : m_x(x), m_y(y), m_power(power) {
+
+}
+
 Basite::~Basite() {
 
 }
@@ -12,9 +16,21 @@ Basite::~Basite() {
 void Basite::draw() const {
     std::cout << "DRAW BASITE" << std::endl;
 
-    sf::RectangleShape rectangle(sf::Vector2f(ENTITY_WIDTH, ENTITY_HEIGHT));
+    unsigned pos_x = ENTITY_WIDTH * m_x;
+    unsigned pos_y = ENTITY_HEIGHT * m_y;
 
+    sf::RectangleShape rectangle(sf::Vector2f(ENTITY_WIDTH, ENTITY_HEIGHT));
+    rectangle.move(pos_x, pos_y);
     rectangle.setFillColor(::Color::Grey);
 
+    unsigned power = MAX_POWER + (MAX_POWER - m_power);
+
+    sf::RectangleShape foreground(sf::Vector2f(ENTITY_WIDTH - power, ENTITY_HEIGHT - power));
+    foreground.setOrigin(sf::Vector2f((ENTITY_WIDTH - power) / 2, (ENTITY_HEIGHT - power) / 2));
+    foreground.move(pos_x + MAX_POWER, pos_y + MAX_POWER);
+
+    foreground.setFillColor(::Color::White);
+
     Game::m_main_window.draw(rectangle);
+    Game::m_main_window.draw(foreground);
 }
--- src/Map.cpp
+++ src/Map.cpp
@@ -1,8 +1,32 @@
 #include "Map.hpp"
 
-void Map::draw() const {
+Map::Map() {
+    // TODO : Tempory
+    unsigned MAP_HEIGHT = 30;
+    unsigned MAP_WIDTH = 30;
+    unsigned max = 8;
+    unsigned min = 1;
+
+    srand(time(NULL)); // initialisation de rand
+
+    for (unsigned i = 0; i < MAP_WIDTH; ++i) {
+        for (unsigned j = 0; j < MAP_HEIGHT; ++j) {
+            unsigned power = rand()%(max-min) + min;
+            m_entities.push_back(new Basite(i, j, power));
+        }
+    }
+}
+
+Map::~Map() {
     for(unsigned i = 0; i < m_entities.size(); ++i) {
-        Entity* entity = m_entities[i];
+        delete m_entities[i];
+        m_entities[i] = 0;
+    }
+}
+
+void Map::draw() const {
+    for (unsigned i = 0; i < m_entities.size(); ++i) {
+        Entity *entity = m_entities[i];
         entity->draw();
     }
 }
